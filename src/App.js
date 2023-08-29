dbd0d9a98e5d7e95858e33042b310f443e73799d
Shawn Ning <shawnningjiale@gmail.com>
2023-08-29 10:46:01 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Title } from "@mantine/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Title align="center">Netflix</Title>
+      <h2>Enjoy big movies, hit series and more from RM17.</h2>
+      {/* list all the movies here */}
+    </Container>
   );
 }
 
